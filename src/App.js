0573f06f927f2f178fa98ee2329e6dd12b9f78df
mensafri <safrijawad1245@gmail.com>
2023-04-15 20:04:24 +0700

    setting2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from "react-bootstrap";
+import "./App.css";
+import NavbarComponent from "./components/NavbarComponent";
+import ListCategories from "./components/ListCategories";
+import Hasil from "./components/Hasil";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h2>Daftar Produk</h2>
+              <br />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
